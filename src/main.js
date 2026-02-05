ef20c6ba8e6c81d7189349f63f0a62b026411830
pureza-discord <tintadepintar@gmail.com>
2026-02-05 15:40:29 -0300

    Update Spotify embed height and URL.

--- src/main.js
+++ src/main.js
@@ -72,9 +72,9 @@ app.innerHTML = `
         <iframe
           data-testid="embed-iframe"
           style="border-radius: 12px"
-          src="https://open.spotify.com/embed/track/0WKqlUwMcxvJj8HS5Kah7Q?utm_source=generator&theme=0"
+          src="https://open.spotify.com/embed/track/0WKqlUwMcxvJj8HS5Kah7Q?utm_source=generator"
           width="100%"
-          height="152"
+          height="352"
           frameborder="0"
           allowfullscreen=""
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
